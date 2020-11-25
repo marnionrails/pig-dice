e7c8b0a5fa0adb83ba7793c8f703595a7a63e464
Marni <suchermarni@gmail.com>
2020-11-25 00:14:34 -0500

    create Player constructor

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+function Player(name) {
+  this.name = name;
+  this.turnScore = 0;
+  this.totalScore = 0;
+}
+
+/* let playerOne;
+let playerTwo;
+let currentPlayer;
+
+$(document).ready(function(event) {
+    // event.preventDefault();
+    playerOne = new Player(prompt("Player one, please enter your name:"));
+    playerTwo = new Player(prompt("Player two, please enter your name:"));
+    console.log(playerOne.name);
+    console.log(playerTwo.name);
+    
+    currentPlayer = playerOne;
+    currentPlayer.turn();
+
+}); */
